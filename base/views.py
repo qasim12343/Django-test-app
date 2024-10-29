30253c1c90b30ecf8de4690e35c5bc428f26d1d1
Qasem <qasem.yousifi20@gmail.com>
2024-10-29 20:17:09 +0330

    initialized room and home

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+rooms = [{'id':1, 'name':'room1'},{'id':2, 'name':'room1'},{'id':3, 'name':'room3'}]
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'rooms':rooms})
 
 def room(request):
     return render(request, 'room.html')
