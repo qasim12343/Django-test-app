88b1297ef16d56b64ec81fd13079d53fcd3d1166
Qasim Yusofi <qasem.yousifi20@gmail.com>
2024-11-03 22:47:27 +0330

    using query of django for filtering and get an specific item

--- base/views.py
+++ base/views.py
@@ -7,9 +7,6 @@ def home(request):
     return render(request, 'base/home.html',{'rooms':rooms})
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room  = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html',context)
