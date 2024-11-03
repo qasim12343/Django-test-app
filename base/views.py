3149753fab775ea8e92b8d0e125785fe8dd5de2f
Qasim Yusofi <qasem.yousifi20@gmail.com>
2024-11-03 22:44:28 +0330

    get data from database(models)

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Room
 rooms = [{'id':1, 'name':'room1'},{'id':2, 'name':'room1'},{'id':3, 'name':'room3'}]
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html',{'rooms':rooms})
 
 def room(request,pk):
